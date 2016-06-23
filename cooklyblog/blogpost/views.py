c12ecc16b78ab3c10aec604662b42832b962c988
Watt Iamsuri <watt@cookly.me>
2016-06-23 16:15:22 +0700

    create html index

--- cooklyblog/blogpost/views.py
+++ cooklyblog/blogpost/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from blogpost.models import BlogPost
 
 # Create your views here.
+
+
+def homepage(request):
+    blog_list = BlogPost.objects.all()
+    data = {
+        'data1': 'hello',
+        'data2': 'its me',
+        'data_blog': blog_list,
+    }
+    return render_to_response('index.html', data)
--- cooklyblog/cooklyblog/urls.py
+++ cooklyblog/cooklyblog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blogpost.views import homepage
 
 urlpatterns = [
+    url(r'^$', homepage, name='Home'),
     url(r'^admin/', admin.site.urls),
 ]
