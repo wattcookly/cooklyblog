6101310c869197be9adbfdfeca9fbc30131cbccb
Watt Iamsuri <watt@cookly.me>
2016-06-23 17:21:46 +0700

    modify admin

--- cooklyblog/blogpost/admin.py
+++ cooklyblog/blogpost/admin.py
@@ -6,7 +6,6 @@ from blogpost.models import BlogPost
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ['title', 'create_at', 'update_at']
-    list_filter = ['create_at']
     search_fields = ['title']
 
 admin.site.register(BlogPost, BlogPostAdmin)
