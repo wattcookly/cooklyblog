a775f23901f06c1fc9b37110ba4e6477c5a60ab5
Watt Iamsuri <watt@cookly.me>
2016-06-24 15:26:17 +0700

    delete logger on view

--- cooklyblog/blogpost/views.py
+++ cooklyblog/blogpost/views.py
@@ -4,21 +4,16 @@ from django.template.context_processors import csrf
 from blogpost.models import BlogPost
 from blogpost.forms import BlogPostForm
 
-# Create your views here.
-import logging
-logger = logging.getLogger(__name__)
-
 
 def homepage(request):
 
+    # if request is post,
     if request.POST:
         # create a form instance and populate it with data from the request:
         form = BlogPostForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            logger.info(form.cleaned_data)
             form.save()
-
             return HttpResponseRedirect('/')
     else:
         form = BlogPostForm()
@@ -29,6 +24,5 @@ def homepage(request):
         'form': form
     }
     data.update(csrf(request))
-    logger.info('hehehehe')
 
     return render_to_response('index.html', data)
